9e461b376f9a59ea8a8bbf0e8fc03747e5446aa4
baishali2502 <baishali2502@gmail.com>
2023-11-24 06:33:28 +0530

    Welcome

--- SnakeLadder/src/SnakeLadder/SnakeLadder.java
+++ SnakeLadder/src/SnakeLadder/SnakeLadder.java
@@ -1,22 +1,26 @@
 package SnakeLadder;
 import java.util.*;
 
-public class SnakeLadder 
+class SnakeNLadder
 {
-	final static int WINPOINT = 100;	
-    
+     //define constant WINPOINT with value 100
+    final static int WINPOINT = 100;
+     
 	/*
-	 * 1.) snake is a static HashMap which stores key as a starting point of snake and
-	 *     value as tailing point of snake.
-	 * 
-	 * 2.) ladder is a static HashMap which stores key as a lower point of ladder 
-	       and value as upper point of ladder.
+	 * snake is a static Hashmap which stores key as a starting point of snake and
+	 * value as tailing point of snake.
 	 */
-	static Map < Integer , Integer > snake = new HashMap < Integer , Integer >();
+    static Map < Integer , Integer > snake = new HashMap < Integer , Integer >();
+	/*
+	 * ladder is a static Hashmap which stores key as a lower point of ladder and
+	 * value as uppoer point of ladder.
+	 */
     static Map < Integer , Integer > ladder = new HashMap< Integer , Integer >();
-    //vlaues of both the Hashmap in static block.
+    
+    
+    //initialized vlaues of both the Hashmap in static block.
     static{
-    	snake.put(99,54);
+        snake.put(99,54);
         snake.put(70,55);
         snake.put(52,42);
         snake.put(25,2);
@@ -28,9 +32,21 @@ public class SnakeLadder
         ladder.put(46,90);
         ladder.put(17,69);
     }
-    
+     
+     int rolls=0;
+     //List of positions of player1 & player2 throughout the game
+     ArrayList<Integer> pos1 = new ArrayList<>();
+     ArrayList<Integer> pos2 = new ArrayList<>();
+}
+
+
+
+public class SnakeLadder 
+{	
     public static void main(String[] args) {
-    	SnakeLadder s = new SnakeLadder();
-    	System.out.println("WELCOME TO SNAKE & LADDER GAME");
+    	SnakeNLadder sn = new SnakeNLadder();
+    	System.out.println("WELCOME TO SNAKE & LADDER GAME\n");
+    	sn.startGame();
+    	sn.report();
     }
 }
