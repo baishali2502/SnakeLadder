de91e46e515543eedf7815f1ec564f5f53b1d400
baishali2502 <baishali2502@gmail.com>
2023-11-24 06:40:13 +0530

    Added:Roll Dice method that creates a random number between 1 to 6

--- SnakeLadder/src/SnakeLadder/SnakeLadder.java
+++ SnakeLadder/src/SnakeLadder/SnakeLadder.java
@@ -96,6 +96,16 @@ class SnakeNLadder
              currentPlayer= -currentPlayer;     
          }
      }
+     
+     
+   //@desc:method is used to generate a random number between 1 to 6.
+ 	public int rollDice() {
+ 		int n = 0;
+ 		Random r = new Random();
+ 		n = r.nextInt(7);
+ 		return (n == 0 ? 1 : n);
+ 	}
+ 	
 }
 
 
